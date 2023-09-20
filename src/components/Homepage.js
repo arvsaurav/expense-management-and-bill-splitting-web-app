700daeb8eb87944f9a9288070f800c3faa61c3f2
Saurav Kumar <arvsaurav@gmail.com>
2023-09-20 17:39:13 +0530

    added styling to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,9 +2,9 @@ import './Homepage.css';
 
 function Homepage() {
     return (
-        <div>
-            <h2>Welcome to our bill splitting app.</h2>
-            <p>Here, you can manage your daily expense and split the bills among friends.</p>
+        <div className='homeDiv'>
+            <h2 className='homeH2'>Welcome to our bill splitting app.</h2>
+            <p className='homeP'>Here, you can manage your daily expense and split the bills among friends.</p>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,10 @@ import './Navbar.css';
 function Navbar() {
     return (
         <div>
-            <ul>
-                <li>Login</li>
-                <li>Register</li>
+            <ul className='navUl'>
+                <li className='navLi' style={{float: 'left'}}>Home</li>
+                <li className='navLi' style={{marginRight: '1.5vw'}}>Login</li>
+                <li className='navLi'>Register</li>
             </ul>
         </div>
     );
