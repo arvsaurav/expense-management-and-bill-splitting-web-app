cc55b26123d97f0ffa82f08ad1cccd3f4c69a3d6
Saurav Kumar <arvsaurav@gmail.com>
2023-10-15 02:32:16 +0530

    added up/down icon

--- src/components/ManageExpense/ManageExpense.js
+++ src/components/ManageExpense/ManageExpense.js
@@ -2,26 +2,34 @@ import { useState } from "react";
 import "./ManageExpense.css";
 import ShowPersonalExpense from "./ShowPersonalExpense";
 import ShowSplittedBills from "./ShowSplittedBills";
+import DownIcon from "../../images/caret-down-solid.svg";
+import UpIcon from "../../images/caret-up-solid.svg";
 
 function ManageExpense({userState}) {
 
     const [personalExpenseVisibility, setPersonalExpenseVisibility] = useState('none');
     const [splittedBillsVisibility, setSplittedBillsVisibility] = useState('none');
+    const [iconPersonalExpense, setIconPersonalExpense] = useState(DownIcon);
+    const [iconSplittedBills, setIconSplittedBills] = useState(DownIcon);
 
     const setVisibility = (id) => {
         if(id === "collapsible1") {
             if(personalExpenseVisibility === 'none') {
+                setIconPersonalExpense(UpIcon);
                 setPersonalExpenseVisibility('block');
             }
             else {
+                setIconPersonalExpense(DownIcon);
                 setPersonalExpenseVisibility('none');
             }
         }
         if(id === "collapsible2") {
             if(splittedBillsVisibility === 'none') {
+                setIconSplittedBills(UpIcon);
                 setSplittedBillsVisibility('block');
             }
             else {
+                setIconSplittedBills(DownIcon);
                 setSplittedBillsVisibility('none');
             }
         }
@@ -31,14 +39,20 @@ function ManageExpense({userState}) {
         <div className="manage-expense-div">
             <h1>Manage Expense</h1>
             <div>
-                <button className="collapsible" onClick={() => {setVisibility("collapsible1")}}>View Personal Expense</button>
+                <button className="collapsible" onClick={() => {setVisibility("collapsible1")}}>
+                    View Personal Expense 
+                    <img id="up-down-icon" alt="icon" src={iconPersonalExpense} />
+                </button>
                 <div className="view-personal-expense" style={{display: `${personalExpenseVisibility}`}}>
                     <div className="content">
                         <ShowPersonalExpense userState={userState} />
                     </div>
                 </div>
                 <br/>
-                <button className="collapsible" onClick={() => {setVisibility("collapsible2")}}>View Bills Splitted With Friends</button>
+                <button className="collapsible" onClick={() => {setVisibility("collapsible2")}}>
+                    View Bills Splitted With Friends
+                    <img id="up-down-icon" alt="icon" src={iconSplittedBills} />
+                </button>
                 <div className="view-bills-splitted-with-friends" style={{display: `${splittedBillsVisibility}`}}>
                     <div className="content">    
                         <ShowSplittedBills userState={userState} />
